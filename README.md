45b4a237a682d6d94fb79a784d048dc086e50c34
Brent Williams <brent.williams@mirumagency.com>
2019-04-09 14:07:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# Motion Activated Sound Player
+This simple project builds a motion activated sound player.  Each time motion is sensed, an MP3 file is played.  I built this originally to play a random sound for my B1 Battle Droid (from the awesome Sean Fields at Project842...look him up on Facebook!), but it can be used in pretty much any project that requires motion detection.
+
+The following components are needed:
+
+* Arduino (Uno or Nano for the 5V output pin)
+* PIR (Passive Infrared) sensor such as HC-SR501 or HC-SR505
+* DFPlayer Mini (for MP3) and Micro SD card
+* Amp board or circuit (such as a PAM8403)
+* Speaker
+
+The DFPlayer has a built-in amp, but in a crowded room it might not be loud enough. Fortunately, it also has two (2) DAC outputs that you can run to an amplifier.
+
+## Setup
+Follow the wiring diagram to build the sensing circuit. Then edit, compile and load the Arduino sketch.
+
+## Sketch Modifications
+You'll need to edit a couple settings in the sketch to match your setup:
+
+```byte maxRnd = 20;```
+
+This is the maximum number of files your have loaded on your SD card.  For example, with this setting, your MP3 files should be named sequentially from 0001.mp3 to 0020.mp3.
+
+```byte volume = 30;```
+
+This is the volume for the DFPlayer.  It affects BOTH the speaker lines out AND the DAC lines.  You could also add a potentiometer and modify the sketch to read that and set the volume, but if you add an amplifier board with a volume control, just set this to 30 (max) and use the volume control on the amplifier.
+
+## Adding the Sound Files
+The DFPlayer is a great mult-purpose MP3 player and is easy to use, but it DOES have some quirks:
+* Files must be named with leading zeros. *For example: 0001.mp3 or 0020.mp3*
+* Despite the name of the file, DFPlayer will index the files according to the timestamp on the file, so you have to copy the files to the SD card in order from 0001 to ?? so that they are indexed the same as their file name.
+* If you copy the files using a Mac, it will also copy some hidden files that you'll need to clean by running:
+
+    ```dot_clean /Volumes/{name of your SD card}```
+
+## Looping or Random
+By default, the sketch uses the ```maxRnd``` variable to calculate a range (from 1 to maxRnd) and plays a random sound each time the sensor is triggered, being careful not to play the last sound again.
+
+Alternatively, you can comment out this portion of the code and uncomment the line:
+
+```DFPlayer.next();```
+
+to just keep looping through the sounds in order each time the sensor is triggered.
+
+## Notes on the PIR
+Depending upon the sensor you use, some things to keep in mind:
+* For the smaller HC-SR505 sensor, you have a much smaller sensor area, but a shorter warmup time.
+* For the larger HC-SR501, you have a much larger sensor area and a longer warmup time (1 to 3 minutes depending on the brand, etc.) and can adjust both the sensitivity and the delay time until the sensor resets.  You should keep the jumper set to single trigger.
+
+For both sensors, once it is triggered, it will report that it's being triggered for several seconds, then go into a standby mode, then reset after a set number of seconds.
+
+The sketch takes this into account so that only the very first report of a trigger is used to kick off the sound play, and then once the sound is finished, a flag is reset to let the the sketch know it's ready to play another file when the sensor is tripped.  If the sensor is tripped or is still reporting being tripped while a sound is playing, it will be ignored. This stops a bunch of sounds from being played at once due to the repeated reports of the sensor being triggered.
+
+
---
+++ motion.ino
@@ -0,0 +1,203 @@
+/*
+ * Simple motion sensor sketch.
+ * Apr '19
+ * Brent Williams (TK-81113)
+ * Becauseinterwebs.com/tktalkie.com
+ * 
+ * This is a simple sketch that uses a PIR (Passive Infrared Resistor) 
+ * motion sensor to trigger an .mp3 file to play user a DFPlayer mini 
+ * mp3 player.
+ * 
+ * The DFPlayer has a built-in amplifier so you can hook speakers up 
+ * directly to it, but in a loud, crowded room it may not be enough, 
+ * so you can run the DAC channels out from the DFPlayer to an amplifier
+ * circuit/board such as a PAM8403 to get much louder volumes.
+ * 
+ * This sketch is basically a combination of an Adafruit sample for the PIR and  
+ * of the sample sketch provided by DFRobot and then slightly modified, 
+ * so I'm not taking credit for it :)
+ * 
+ * https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299?gclid=CjwKCAjw-ZvlBRBbEiwANw9UWk0FDywBGz_ngc-lFUCtihJJHgaZcOVHAfy_BC2OBnvwC9r4t_ifXRoCt0cQAvD_BwE
+ */
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial DFSerial(10, 11);            // RX, TX; Communicate with the DFPlayer
+DFRobotDFPlayerMini DFPlayer;               // DFPlayer library
+void checkState(uint8_t type, int value);   // Used to reset the play flag and output status messages
+
+int ledPin = 13;                // choose the pin for the LED (use built-in by default)
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int val = 0;                    // variable for reading the pin status
+int pirState = LOW;             // track state of PIR module
+boolean canPlay = true;         // flag to let us know when the next mp3 can be played since the PIR module will output
+                                // HIGH for several seconds, but we only want the first notice to trigger an mp3 playback.
+byte  lastRnd;                  // holds last random number so that the same file is not played twice in a row
+
+// SETTINGS YOU SHOULD CHANGE
+byte  maxRnd = 20;              // Set this to the max number of mp3 files you want to be played
+byte  volume = 30;              // Set this to the volume level you want for the DFPlayer.  This affects BOTH the 
+                                // speaker out AND the DAC lines!  Values from 0 - 30.  You could also hook up a 
+                                // potentiometer to control volume and modify the sketch to read the pot.
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+  digitalWrite (inputPin, LOW);
+  DFSerial.begin(9600);
+  Serial.begin(9600);
+  // Use softwareSerial to communicate with mp3.
+  if (!DFPlayer.begin(DFSerial)) {  
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  DFPlayer.volume(volume);  //Set volume value. From 0 to 30. This affects the DAC output as well!
+
+  // short delay between commands...
+  delay(30);
+  
+  // Here you can set a different EQ on the DFPlayer to one that sounds best with your speaker setup.
+  // DFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  // DFPlayer.EQ(DFPLAYER_EQ_POP);
+  // DFPlayer.EQ(DFPLAYER_EQ_ROCK);
+  // DFPlayer.EQ(DFPLAYER_EQ_JAZZ);
+  // DFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
+  // DFPlayer.EQ(DFPLAYER_EQ_BASS);
+
+  delay(30);
+  
+  canPlay = false;
+  
+  playTrack(1);     //Play the first mp3
+
+  delay(30);
+  
+}
+
+void loop(){
+
+  val = digitalRead(inputPin);   // read input value from PIR
+
+  switch (val) {
+    case HIGH:
+      {
+        digitalWrite(ledPin, HIGH);  // turn LED ON
+        if (pirState == LOW && canPlay == true) {
+          // we have just turned on
+          Serial.println("Motion detected!");
+          canPlay = false;
+          // Begin Random -----------------------------
+          byte rnd = 0;
+          byte count = 0;
+          rnd = lastRnd;
+          while (rnd == lastRnd && count < 50) { 
+            rnd = random(1, maxRnd);
+            count++;
+          }
+          lastRnd = rnd;
+          playTrack(rnd);
+          // End Random -------------------------------
+          // Comment out the random section above and uncomment the line 
+          // below to just loop through the sounds in sequential order.
+          //DFPlayer.next();
+          pirState = HIGH;
+        }
+      }
+      break;
+    case LOW:
+      {
+        if (pirState == HIGH){
+          digitalWrite(ledPin, LOW); // turn LED OFF
+          Serial.println("Motion ended!");
+          pirState = LOW;
+        }
+      }
+      break;
+  }
+
+  // Check state of DFPlayer 
+  // The 'canPlay' flag is reset when the current mp3 ends so that the next one can be played when motion is detected.
+  if (DFPlayer.available()) {
+    checkState(DFPlayer.readType(), DFPlayer.read()); 
+  }
+  
+}
+
+// Play the specified track
+void playTrack(uint8_t track) {
+   DFPlayer.play(track);
+   delay(30);
+   int file = DFPlayer.readCurrentFileNumber();
+
+   Serial.print("Track:");Serial.println(track);
+   Serial.print("File:");Serial.println(file);
+
+   //while (file != track) {
+   //  DFPlayer.play(track);
+   //  delay(200);
+   //  file = DFPlayer.readCurrentFileNumber();
+  // }
+}
+// Check the current state of DFPlayer
+void checkState(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      canPlay = true;
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
